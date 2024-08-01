ea27b5e57b14d3dc25ca02b1fe09ad64a2cfafd5
Łukasz <luxus0@gmail.com>
2024-08-01 11:19:11 +0200

    Add Global Exception handler ResourceNotFoundException

---
+++ src/main/java/com/github/github_repo_info/infrastructure/errorhanler/GithubRepositoryErrorHandlerClient.java
@@ -0,0 +1,17 @@
+package com.github.github_repo_info.infrastructure.errorhanler;
+
+import com.github.github_repo_info.domain.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GithubRepositoryErrorHandlerClient {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<GithubErrorResponseDto> handleResourceNotFoundException(ResourceNotFoundException exception) {
+        GithubErrorResponseDto errorResponse = new GithubErrorResponseDto(HttpStatus.NOT_FOUND.value(), exception.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+}
