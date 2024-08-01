ffc49f481bb0af11604199ab9900b2b436508698
Łukasz <luxus0@gmail.com>
2024-08-01 11:37:35 +0200

    Rename BranchInfo to Branch

---
+++ src/main/java/com/github/github_repo_info/domain/Branch.java
@@ -0,0 +1,4 @@
+package com.github.github_repo_info.domain;
+
+public record Branch(String name, String lastCommitSha) {
+}
--- src/main/java/com/github/github_repo_info/domain/BranchInfo.java
+++
@@ -1,4 +0,0 @@
-package com.github.github_repo_info.domain;
-
-public record BranchInfo(String name, String lastCommitSha) {
-}
