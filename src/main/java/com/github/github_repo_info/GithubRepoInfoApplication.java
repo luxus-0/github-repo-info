491ae3b73252ed4c61e8c68e69d338829a3182d2
Łukasz <luxus0@gmail.com>
2024-08-01 11:23:10 +0200

    Add @EnableConfigurationProperties to GithubRepoInfoApplication

--- src/main/java/com/github/github_repo_info/GithubRepoInfoApplication.java
+++ src/main/java/com/github/github_repo_info/GithubRepoInfoApplication.java
@@ -1,9 +1,12 @@
 package com.github.github_repo_info;
 
+import com.github.github_repo_info.domain.GithubApiConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(GithubApiConfigurationProperties.class)
 public class GithubRepoInfoApplication {
 
 	public static void main(String[] args) {
