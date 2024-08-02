4cda8b8928a266a0b453fcd8644e41c5e1b495b9
Łukasz <luxus0@gmail.com>
2024-08-02 09:41:37 +0200

    Refactor Controller change GithubRepositoryClient to GithubService

--- src/main/java/com/github/github_repo_info/infrastructure/client/GithubController.java
+++ src/main/java/com/github/github_repo_info/infrastructure/client/GithubController.java
@@ -1,5 +1,6 @@
 package com.github.github_repo_info.infrastructure.client;
 
+import com.github.github_repo_info.domain.GithubService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/github")
-public class GithubRepositoryClientController {
+public class GithubController {
 
-    private final GithubRepositoryClient githubRepositoryClient;
+    private final GithubService githubService;
 
     @GetMapping("/repos/{username}")
-    public ResponseEntity<?> getUserRepositories(@PathVariable String username) {
-        return ResponseEntity.ok(githubRepositoryClient.fetchRepositories(username));
+    public ResponseEntity<?> getRepositories(@PathVariable String username) {
+        return ResponseEntity.ok(githubService.findReposWithBranches(username));
     }
 }
