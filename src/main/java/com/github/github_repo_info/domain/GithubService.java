707c1bd9b9714f291b39ee96a33bd28a4593cba0
Łukasz <luxus0@gmail.com>
2024-08-03 21:04:53 +0200

    refactor: Refactor method findRepositories
    Create private method

--- src/main/java/com/github/github_repo_info/domain/GithubService.java
+++ src/main/java/com/github/github_repo_info/domain/GithubService.java
@@ -1,10 +1,12 @@
 package com.github.github_repo_info.domain;
 
+import com.github.github_repo_info.domain.exception.ResourceNotFoundException;
 import com.github.github_repo_info.infrastructure.client.GithubRepositoryClient;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @AllArgsConstructor
@@ -12,14 +14,35 @@ public class GithubService {
 
     private final GithubRepositoryClient githubClient;
 
-    public List<RepositoryInfo> findReposWithBranches(String username) {
+    public List<RepositoryInfo> findRepositories(String username) {
+        if(username == null){
+            throw new IllegalArgumentException("Repository username not found");
+        }
+        RepositoryInfo repositoryInfo = mapToRepository(username);
+        return List.of(repositoryInfo);
+        }
+
+        public CompletableFuture<List<RepositoryInfo>> findRepositoriesAsync(String username) {
+            return CompletableFuture.supplyAsync(() -> {
+                if (username == null) {
+                    throw new IllegalArgumentException("Repository username not found");
+                }
+                RepositoryInfo repository = mapToRepository(username);
+                return List.of(repository);
+            });
+        }
+
+    private RepositoryInfo mapToRepository(String username) {
         List<Repository> repositories = githubClient.findRepositories(username);
         return repositories.stream()
                 .filter(repository -> !repository.fork())
-                .map(repository -> {
-                    List<Branch> branches = githubClient.findBranches(username, repository.name());
-                    return new RepositoryInfo(repository.name(), repository.login(), branches);
-                })
-                .toList();
+                .map(repository -> createRepository(username, repository))
+                .findAny()
+                .orElseThrow(() -> new ResourceNotFoundException("Repository not found"));
+    }
+
+    private RepositoryInfo createRepository(String username, Repository repository) {
+        List<Branch> branches = githubClient.findBranches(username, repository.name());
+        return new RepositoryInfo(repository.name(), repository.login(), branches);
     }
 }
