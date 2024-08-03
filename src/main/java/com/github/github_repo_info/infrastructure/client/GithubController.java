b4b1e9fec296d3ecd57dbb0559fc300a0d8a3b7f
Łukasz <luxus0@gmail.com>
2024-08-03 13:17:10 +0200

    feat: enhance getRepositories endpoint to return typed list of RepositoryInfo
    Refactor `GithubController` to improve type safety and clarity by changing the return type of the `getRepositories` method.
    
    - Before: The method returned a generic `ResponseEntity<?>`.
    - After: The method now returns `ResponseEntity<List<RepositoryInfo>>`.
    
    This change ensures that the response contains a list of `RepositoryInfo` objects, enhancing type safety and providing better context for the response structure.

--- src/main/java/com/github/github_repo_info/infrastructure/client/GithubController.java
+++ src/main/java/com/github/github_repo_info/infrastructure/client/GithubController.java
@@ -1,12 +1,15 @@
 package com.github.github_repo_info.infrastructure.client;
 
 import com.github.github_repo_info.domain.GithubService;
+import com.github.github_repo_info.domain.RepositoryInfo;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -16,7 +19,7 @@ public class GithubController {
     private final GithubService githubService;
 
     @GetMapping("/repos/{username}")
-    public ResponseEntity<?> getRepositories(@PathVariable String username) {
+    public ResponseEntity<List<RepositoryInfo>> getRepositories(@PathVariable String username) {
         return ResponseEntity.ok(githubService.findReposWithBranches(username));
     }
 }
